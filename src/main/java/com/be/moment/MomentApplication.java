16ffa380750af46eb47b680488414276e2922cb2
seokscirbe <seokscribe1970@gmail.com>
2023-05-20 23:16:02 +0900

    Init: 프로젝트 초기 생성

---
+++ src/main/java/com/be/moment/MomentApplication.java
@@ -0,0 +1,13 @@
+package com.be.moment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/be/moment/MomentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.moment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
